fc0d612c75dc7295a6668fab7cf37e5ef8ee282b
Antonio <aolmolopez97@gmail.com>
2025-11-07 16:16:10 +0100

    Update design

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'app-footer',
@@ -8,6 +9,14 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class FooterComponent {
   currentYear = new Date().getFullYear();
+  isDarkMode = false;
 
-  constructor(private translate: TranslateService) {}
+  constructor(
+    private translate: TranslateService,
+    public themeService: ThemeService
+  ) {
+    this.themeService.darkMode$.subscribe(isDark => {
+      this.isDarkMode = isDark;
+    });
+  }
 }
