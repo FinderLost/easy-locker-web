d2a747b992eccfc53a26688aba068b0119dd82ef
Andrés Felipe <aalzres@gmail.com>
2025-11-13 23:28:32 +0100

    feat(faq): actualiza preguntas y traducciones en todos los idiomas

--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -8,7 +8,15 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class FaqComponent {
   faqs = [
-    { questionKey: 'faq_q1', answerKey: 'faq_a1', isOpen: false }
+    { questionKey: 'faq_q1', answerKey: 'faq_a1', isOpen: false },
+    { questionKey: 'faq_q2', answerKey: 'faq_a2', isOpen: false },
+    { questionKey: 'faq_q3', answerKey: 'faq_a3', isOpen: false },
+    { questionKey: 'faq_q4', answerKey: 'faq_a4', isOpen: false },
+    { questionKey: 'faq_q5', answerKey: 'faq_a5', isOpen: false },
+    { questionKey: 'faq_q6', answerKey: 'faq_a6', isOpen: false },
+    { questionKey: 'faq_q7', answerKey: 'faq_a7', isOpen: false },
+    { questionKey: 'faq_q8', answerKey: 'faq_a8', isOpen: false },
+    { questionKey: 'faq_q9', answerKey: 'faq_a9', isOpen: false }
   ];
 
   constructor(private translate: TranslateService) {}
