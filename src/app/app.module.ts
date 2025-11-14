8cdb92ccbddd8072d648d93de517ed48231bc71f
Andrés Felipe <aalzres@gmail.com>
2025-11-14 11:33:51 +0100

    chore(cleanup): elimina LocationComponent y sus archivos porque no se usa en el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient } from '@angular/common/http';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
-import { LocationComponent } from './components/location/location.component';
 import { FaqComponent } from './components/faq/faq.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LanguageSwitcherComponent } from './components/language-switcher/language-switcher.component';
@@ -26,7 +25,6 @@ export function HttpLoaderFactory(http: HttpClient) {
     AppComponent,
     HeaderComponent,
     HeroComponent,
-    LocationComponent,
     FaqComponent,
     FooterComponent,
     LanguageSwitcherComponent,
