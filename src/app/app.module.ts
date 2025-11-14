dc4de7860bf0f00d769afe16b9e8a4c0ad44cb1a
Andrés Felipe <aalzres@gmail.com>
2025-11-14 11:13:13 +0100

    chore(cleanup): elimina FeaturesComponent y sus archivos porque no se usa en el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { HowItWorksComponent } from './components/how-it-works/how-it-works.component';
 import { LocationComponent } from './components/location/location.component';
-import { FeaturesComponent } from './components/features/features.component';
 import { FaqComponent } from './components/faq/faq.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LanguageSwitcherComponent } from './components/language-switcher/language-switcher.component';
@@ -30,7 +29,6 @@ export function HttpLoaderFactory(http: HttpClient) {
     HeroComponent,
     HowItWorksComponent,
     LocationComponent,
-    FeaturesComponent,
     FaqComponent,
     FooterComponent,
     LanguageSwitcherComponent,
