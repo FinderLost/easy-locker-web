00ff1fa94a595e6e863b527269a3b666a3f4b4ee
Andrés Felipe <45148277+aalzres@users.noreply.github.com>
2025-12-09 19:25:22 +0100

    SEO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { LanguageService } from './services/language.service';
 import { Title, Meta } from '@angular/platform-browser';
 import { TranslateService } from '@ngx-translate/core';
 import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { filter, takeUntil } from 'rxjs/operators';
 import { CookieConsentService } from './core/services/cookie-consent.service';
 
 @Component({
@@ -20,12 +21,19 @@ export class AppComponent implements OnInit, OnDestroy {
     private titleService: Title,
     private metaService: Meta,
     private translate: TranslateService,
+    private router: Router,
     public cookieConsent: CookieConsentService
   ) {}
 
   ngOnInit(): void {
     this.languageService.init();
     this.updateSeo();
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        takeUntil(this.destroy$)
+      )
+      .subscribe(() => this.updateSeo());
     this.translate.onLangChange
       .pipe(takeUntil(this.destroy$))
       .subscribe(() => this.updateSeo());
@@ -39,12 +47,14 @@ export class AppComponent implements OnInit, OnDestroy {
   private updateSeo(): void {
     const seoTitle = this.translate.instant('seo.home.title');
     const seoDescription = this.translate.instant('seo.home.description');
+    const seoKeywords = this.translate.instant('seo.home.keywords');
 
     this.titleService.setTitle(seoTitle);
     this.metaService.updateTag({
       name: 'description',
       content: seoDescription,
     });
+    this.metaService.updateTag({ name: 'keywords', content: seoKeywords });
     this.metaService.updateTag({ property: 'og:title', content: seoTitle });
     this.metaService.updateTag({
       property: 'og:description',
